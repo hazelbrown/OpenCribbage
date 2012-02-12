2a803a0519924b147e900c21ea58c2f87f56bcc3
Neil Parikh <neilp1@facebook.com>
2012-02-12 12:42:56 -0700

    Added new deck format, using dictionaries

---
+++ deck_of_cards.py
@@ -0,0 +1,27 @@
+# cleaner deck, uses a dictionary instead, but I'm keeping the old array just in case
+deck = [{"number": 101, "suit": "clubs", "value": "Ace"}, {"number": 102, "suit": "clubs", "value": "Two"}, \
+        {"number": 103, "suit": "clubs", "value": "Three"}, {"number": 104, "suit": "clubs", "value": "Four"}, \
+        {"number": 105, "suit": "clubs", "value": "Five"}, {"number": 106, "suit": "clubs", "value": "Six"}, \
+        {"number": 107, "suit": "clubs", "value": "Seven"}, {"number": 108, "suit": "clubs", "value": "Eight"}, \
+        {"number": 109, "suit": "clubs", "value": "Nine"}, {"number": 110, "suit": "clubs", "value": "Ten"}, \
+        {"number": 111, "suit": "clubs", "value": "Jack"}, {"number": 112, "suit": "clubs", "value": "Queen"}, \
+        {"number": 113, "suit": "clubs", "value": "King"}, {"number": 201, "suit": "spades", "value": "Ace"}, \
+        {"number": 202, "suit": "spades", "value": "Two"}, {"number": 203, "suit": "spades", "value": "Three"}, \
+        {"number": 204, "suit": "spades", "value": "Four"}, {"number": 205, "suit": "spades", "value": "Five"}, \
+        {"number": 206, "suit": "spades", "value": "Six"}, {"number": 207, "suit": "spades", "value": "Seven"}, \
+        {"number": 208, "suit": "spades", "value": "Eight"}, {"number": 209, "suit": "spades", "value": "Nine"}, \
+        {"number": 210, "suit": "spades", "value": "Ten"}, {"number": 211, "suit": "spades", "value": "Jack"}, \
+        {"number": 212, "suit": "spades", "value": "Queen"}, {"number": 213, "suit": "spades", "value": "King"}, \
+        {"number": 301, "suit": "hearts", "value": "Ace"}, {"number": 302, "suit": "hearts", "value": "Two"}, \
+        {"number": 303, "suit": "hearts", "value": "Three"}, {"number": 304, "suit": "hearts", "value": "Four"}, \
+        {"number": 305, "suit": "hearts", "value": "Five"}, {"number": 306, "suit": "hearts", "value": "Six"}, \
+        {"number": 307, "suit": "hearts", "value": "Seven"}, {"number": 308, "suit": "hearts", "value": "Eight"}, \
+        {"number": 309, "suit": "hearts", "value": "Nine"}, {"number": 310, "suit": "hearts", "value": "Ten"}, \
+        {"number": 311, "suit": "hearts", "value": "Jack"}, {"number": 312, "suit": "hearts", "value": "Queen"}, \
+        {"number": 313, "suit": "hearts", "value": "King"}, {"number": 401, "suit": "diamonds", "value": "Ace"}, \
+        {"number": 402, "suit": "diamonds", "value": "Two"}, {"number": 403, "suit": "diamonds", "value": "Three"}, \
+        {"number": 404, "suit": "diamonds", "value": "Four"}, {"number": 405, "suit": "diamonds", "value": "Five"}, \
+        {"number": 406, "suit": "diamonds", "value": "Six"}, {"number": 407, "suit": "diamonds", "value": "Seven"}, \
+        {"number": 408, "suit": "diamonds", "value": "Eight"}, {"number": 409, "suit": "diamonds", "value": "Nine"}, \
+        {"number": 410, "suit": "diamonds", "value": "Ten"}, {"number": 411, "suit": "diamonds", "value": "Jack"}, \
+        {"number": 412, "suit": "diamonds", "value": "Queen"}, {"number": 413, "suit": "diamonds", "value": "King"}]
